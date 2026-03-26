5af043e0d05de19f83995ef69ff8cc6017228bf0
ye.dyussembayev <ye.dyussembayev@samalsoft.com>
2026-03-26 14:17:42 +0500

    df

--- src/redux/features/baseQueryWithReauth.ts
+++ src/redux/features/baseQueryWithReauth.ts
@@ -6,7 +6,7 @@ import {
 } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'http://localhost:4000',
+	baseUrl: 'http://localhost:4000/api',
 	credentials: 'include'
 })
 
